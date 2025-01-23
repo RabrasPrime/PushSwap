7bce89418321de8ab8b4912e63459697849f0a9c
Tom Jooris <tjooris@z3r5p5.42lyon.fr>
2025-01-23 17:01:53 +0100

    ajout de différentes fonctions

--- src/3/three_number.c
+++ src/3/three_number.c
@@ -6,7 +6,7 @@
 /*   By: tjooris <tjooris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 11:07:46 by tjooris           #+#    #+#             */
-/*   Updated: 2025/01/23 16:22:10 by tjooris          ###   ########.fr       */
+/*   Updated: 2025/01/23 16:59:20 by tjooris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,26 @@
 
 int	check_order(t_stack	*stack)
 {
-	
+	if ((stack->number < stack->next->number)
+		&& (stack->next->number < stack->prev->number))
+		return (1);
+	else if ((stack->number > stack->next->number)
+		&& (stack->next->number < stack->prev->number)
+		&& (stack->prev->number > stack->number))
+		return (2);
+	else if ((stack->number > stack->next->number)
+		&& (stack->next->number > stack->prev->number)
+		&& (stack->prev->number < stack->number))
+		return (3);
+	else if ((stack->number > stack->next->number)
+		&& (stack->next->number < stack->prev->number)
+		&& (stack->prev->number < stack->number))
+		return (4);
+	else if ((stack->number < stack->next->number)
+		&& (stack->next->number > stack->prev->number)
+		&& (stack->prev->number > stack->number))
+		return (5);
+	else
+		return (6);
 }
+
