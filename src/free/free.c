2f80f6c630f8f820211676e8d2b49fcf5c01734a
Tom Jooris <tjooris@z1r7p3.42lyon.fr>
2025-01-31 17:13:02 +0100

    plus que les safes

--- src/free/free.c
+++ src/free/free.c
@@ -1,29 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   free_stack.c                                       :+:      :+:    :+:   */
+/*   free.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tjooris <tjooris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/24 13:24:30 by tjooris           #+#    #+#             */
-/*   Updated: 2025/01/31 12:53:08 by tjooris          ###   ########.fr       */
+/*   Created: 2025/01/31 11:35:05 by tjooris           #+#    #+#             */
+/*   Updated: 2025/01/31 11:56:53 by tjooris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_pushswap.h"
 
-void	ft_freestack(t_stack	**stack, int size)
-{
-	t_stack	*current;
-	int		i;
-
-	i = 0;
-	current = *stack;
-	while ( i < size - 1 )
-	{
-		current = current->next;
-		free(current->prev);
-		i++;
-	}
-	free(current);
-}
--- src/stack/utils.c
+++ src/stack/utils.c
@@ -6,7 +6,7 @@
 /*   By: tjooris <tjooris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 11:53:11 by tjooris           #+#    #+#             */
-/*   Updated: 2025/01/30 16:43:40 by tjooris          ###   ########.fr       */
+/*   Updated: 2025/01/31 13:29:53 by tjooris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,11 @@ static void	add_stack(t_stack *stack, t_stack *add)
 	while (top->next != stack)
 	{
 		if (top->number == add->number)
-			exit(-1) ;
+			exit(-1);
 		top = top->next;
 	}
 	if (top->number == add->number)
-		exit(-1) ;
+		exit(-1);
 	if (stack->next == stack && stack->prev == stack)
 	{
 		stack->next = add;
@@ -101,11 +101,13 @@ t_stack	*stack_init(t_stack	*stack, char **array)
 
 	stack = create_stack(ft_atoi(array[0]));
 	if (!stack)
-		error();
+		ft_error();
 	stack->next = stack;
 	stack->prev = stack;
 	i = 1;
 	while (array[i])
 		add_stack(stack, create_stack(ft_atoi(array[i++])));
+		if (!stack)
+			ft_error();
 	return (stack);
 }
